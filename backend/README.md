1bb2da1411875aa56252369e87a0157cad9b7710
cantstopcoding <jonathan_koss@yahoo.com>
2022-06-03 11:04:27 -0400

    init commit, decided to do monorepo from. desc from frontend-dambrosio-designs and backend-dambrosio-designs

---
+++ backend/README.md
@@ -0,0 +1,1 @@
+# backend-dambrosio-designs
---
+++ frontend/src/components/CheckoutSteps.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+
+export default function CheckoutSteps(props) {
+  return (
+    <Row className='checkout-steps'>
+      <Col className={props.step1 ? 'active' : ''}>Sign-In</Col>
+      <Col className={props.step2 ? 'active' : ''}>Shipping</Col>
+      <Col className={props.step3 ? 'active' : ''}>Payment</Col>
+      <Col className={props.step4 ? 'active' : ''}>Place Order</Col>
+    </Row>
+  );
+}
