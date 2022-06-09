b7bb62eaa229dd89bb97de8ade411b3e255c1183
cantstopcoding <jonathan_koss@yahoo.com>
2022-06-09 13:05:08 -0400

    finish search screen

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -7,51 +7,80 @@ import { Helmet } from 'react-helmet-async';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Rating from '../components/Rating';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import Button from 'react-bootstrap/Button';
+import Product from '../components/Product';
+import LinkContainer from 'react-router-bootstrap/LinkContainer';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        products: action.payload.products,
+        page: action.payload.page,
+        pages: action.payload.pages,
+        countProducts: action.payload.countProducts,
+        loading: false,
+      };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
 
 const prices = [
-  { name: '$1 to $50', value: '1-50' },
-  { name: '$51 to $200', value: '51-200' },
-  { name: '$201 to $1000', value: '201-1000' },
+  {
+    name: '$1 to $50',
+    value: '1-50',
+  },
+  {
+    name: '$51 to $200',
+    value: '51-200',
+  },
+  {
+    name: '$201 to $1000',
+    value: '201-1000',
+  },
 ];
 
-const ratings = [
-  { name: '4stars & up', rating: 4 },
-  { name: '3stars & up', rating: 3 },
-  { name: '2stars & up', rating: 2 },
-  { name: '1star & up', rating: 1 },
+export const ratings = [
+  {
+    name: '4stars & up',
+    rating: 4,
+  },
+
+  {
+    name: '3stars & up',
+    rating: 3,
+  },
+
+  {
+    name: '2stars & up',
+    rating: 2,
+  },
+
+  {
+    name: '1stars & up',
+    rating: 1,
+  },
 ];
 
 export default function SearchScreen() {
   const navigate = useNavigate();
   const { search } = useLocation();
-  const sp = new URLSearchParams(search);
+  const sp = new URLSearchParams(search); // /search?category=Shirts
   const category = sp.get('category') || 'all';
   const query = sp.get('query') || 'all';
   const price = sp.get('price') || 'all';
   const rating = sp.get('rating') || 'all';
-  const order = sp.get('order') || 'all';
+  const order = sp.get('order') || 'newest';
   const page = sp.get('page') || 1;
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'FETCH_REQUEST':
-        return { ...state, loading: true };
-      case 'FETCH_SUCCESS':
-        return {
-          ...state,
-          products: action.payload.products,
-          page: action.payload.page,
-          pages: action.payload.pages,
-          countProducts: action.payload.countProducts,
-          loading: false,
-        };
-      case 'FETCH_FAIL':
-        return { ...state, loading: false, error: action.payload };
-
-      default:
-        return state;
-    }
-  };
 
   const [{ loading, error, products, pages, countProducts }, dispatch] =
     useReducer(reducer, {
@@ -67,7 +96,10 @@ export default function SearchScreen() {
         );
         dispatch({ type: 'FETCH_SUCCESS', payload: data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+        dispatch({
+          type: 'FETCH_FAIL',
+          payload: getError(error),
+        });
       }
     };
     fetchData();
@@ -77,13 +109,14 @@ export default function SearchScreen() {
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const { data } = await axios.get('/api/prroducts/categories');
+        const { data } = await axios.get(`/api/products/categories`);
         setCategories(data);
       } catch (err) {
         toast.error(getError(err));
       }
     };
-  });
+    fetchCategories();
+  }, [dispatch]);
 
   const getFilterUrl = (filter) => {
     const filterPage = filter.page || page;
@@ -94,12 +127,13 @@ export default function SearchScreen() {
     const sortOrder = filter.order || order;
     return `/search?category=${filterCategory}&query=${filterQuery}&price=${filterPrice}&rating=${filterRating}&order=${sortOrder}&page=${filterPage}`;
   };
-
   return (
     <div>
-      <Helmet>Search Products</Helmet>
+      <Helmet>
+        <title>Search Products</title>
+      </Helmet>
       <Row>
-        <Col>
+        <Col md={3}>
           <h3>Department</h3>
           <div>
             <ul>
@@ -162,13 +196,87 @@ export default function SearchScreen() {
               <li>
                 <Link
                   to={getFilterUrl({ rating: 'all' })}
-                  className={'all' === rating ? 'text-bold' : ''}
+                  className={rating === 'all' ? 'text-bold' : ''}
                 >
                   <Rating caption={' & up'} rating={0}></Rating>
                 </Link>
               </li>
             </ul>
           </div>
+        </Col>
+        <Col md={9}>
+          {loading ? (
+            <LoadingBox></LoadingBox>
+          ) : error ? (
+            <MessageBox variant='danger'>{error}</MessageBox>
+          ) : (
+            <>
+              <Row className='justify-content-between mb-3'>
+                <Col md={6}>
+                  <div>
+                    {countProducts === 0 ? 'No' : countProducts} Results
+                    {query !== 'all' && ' : ' + query}
+                    {category !== 'all' && ' : ' + category}
+                    {price !== 'all' && ' : Price ' + price}
+                    {rating !== 'all' && ' : Rating ' + rating + ' & up'}
+                    {query !== 'all' ||
+                    category !== 'all' ||
+                    rating !== 'all' ||
+                    price !== 'all' ? (
+                      <Button
+                        variant='light'
+                        onClick={() => navigate('/search')}
+                      >
+                        <i className='fas fa-times-circle'></i>
+                      </Button>
+                    ) : null}
+                  </div>
+                </Col>
+                <Col className='text-end'>
+                  Sort by{' '}
+                  <select
+                    value={order}
+                    onChange={(e) => {
+                      navigate(getFilterUrl({ order: e.target.value }));
+                    }}
+                  >
+                    <option value='newest'>Newest Arrivals</option>
+                    <option value='lowest'>Price: Low to High</option>
+                    <option value='highest'>Price: High to Low</option>
+                    <option value='toprated'>Avg. Customer Reviews</option>
+                  </select>
+                </Col>
+              </Row>
+              {products.length === 0 && (
+                <MessageBox>No Product Found</MessageBox>
+              )}
+
+              <Row>
+                {products.map((product) => (
+                  <Col sm={6} lg={4} className='mb-3' key={product._id}>
+                    <Product product={product}></Product>
+                  </Col>
+                ))}
+              </Row>
+
+              <div>
+                {[...Array(pages).keys()].map((x) => (
+                  <LinkContainer
+                    key={x + 1}
+                    className='mx-1'
+                    to={getFilterUrl({ page: x + 1 })}
+                  >
+                    <Button
+                      className={Number(page) === x + 1 ? 'text-bold' : ''}
+                      variant='light'
+                    >
+                      {x + 1}
+                    </Button>
+                  </LinkContainer>
+                ))}
+              </div>
+            </>
+          )}
         </Col>
       </Row>
     </div>
