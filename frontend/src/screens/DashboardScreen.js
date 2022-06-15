61cee1e7cd6be919a8be62c2ea9d74243899c320
cantstopcoding <jonathan_koss@yahoo.com>
2022-06-15 08:36:10 -0400

    create reducer in DashboardScreen

--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, summary: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
 export default function DashboardScreen() {
   return <div>DashboardScreen</div>;
 }
