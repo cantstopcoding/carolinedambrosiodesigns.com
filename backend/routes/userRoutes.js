d7dfab254423271e6b68f8a3ca85ba405a49b854
cantstopcoding <jonathan_koss@yahoo.com>
2022-07-29 10:53:57 -0400

    place /profile in front of /:id to update because updated was hitting /:id

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -31,6 +31,32 @@ userRouter.get(
 );
 
 userRouter.put(
+  '/profile',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    console.log('got it!');
+    const user = await User.findById(req.user._id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      if (req.body.password) {
+        user.password = bcrypt.hashSync(req.body.password, 8);
+      }
+      const updatedUser = await user.save();
+      res.send({
+        _id: updatedUser._id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+        isAdmin: updatedUser.isAdmin,
+        token: generateToken(updatedUser),
+      });
+    } else {
+      res.status(404).send({ message: 'User not found' });
+    }
+  })
+);
+
+userRouter.put(
   '/:id',
   isAuth,
   isAdmin,
@@ -104,31 +130,6 @@ userRouter.post(
       isAdmin: user.isAdmin,
       token: generateToken(user),
     });
-  })
-);
-
-userRouter.put(
-  '/profile',
-  isAuth,
-  expressAsyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id);
-    if (user) {
-      user.name = req.body.name || user.name;
-      user.email = req.body.email || user.email;
-      if (req.body.password) {
-        user.password = bcrypt.hashSync(req.body.password, 8);
-      }
-      const updatedUser = await user.save();
-      res.send({
-        _id: updatedUser._id,
-        name: updatedUser.name,
-        email: updatedUser.email,
-        isAdmin: updatedUser.isAdmin,
-        token: generateToken(updatedUser),
-      });
-    } else {
-      res.status(404).send({ message: 'User not found' });
-    }
   })
 );
 
