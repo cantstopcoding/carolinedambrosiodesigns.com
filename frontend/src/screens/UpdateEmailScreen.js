cef4096db445a6e82216dbd14874e02241c89582
cantstopcoding <jonathan_koss@yahoo.com>
2022-09-01 22:39:56 -0400

    create update email screen

--- frontend/src/screens/UpdateEmailScreen.js
+++ frontend/src/screens/UpdateEmailScreen.js
@@ -1,5 +1,107 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Helmet } from 'react-helmet-async';
+import { toast } from 'react-toastify';
+import { Store } from '../Store';
+import { getError } from '../utils';
 
 export default function UpdateEmailScreen() {
-  return <div>UpdateEmailScreen</div>;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [userConfirmsPassword, setUserConfirmsPassword] = useState(false);
+
+  const confirmPasswordToSeeUserInfoHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        '/api/users/confirm-password-to-see-user-info',
+        {
+          password,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+
+      if (data.message === 'Password is correct') {
+        setUserConfirmsPassword(true);
+      }
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { data } = await axios.post(
+        '/api/users/email-otp',
+        {
+          email,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      console.log('data:', data);
+      toast.success(data.message);
+    } catch (err) {
+      toast.error(getError(err));
+    }
+  };
+
+  return (
+    <div className='container small-container'>
+      <Helmet>
+        <title>Change your email</title>
+      </Helmet>
+      {displayCertainFormHandler()}
+    </div>
+  );
+
+  function displayCertainFormHandler() {
+    return userConfirmsPassword ? newEmailAddressForm() : confirmPasswordForm();
+  }
+
+  function newEmailAddressForm() {
+    return (
+      <form onSubmit={submitHandler}>
+        <Form.Group className='mb-3' controlId='email'>
+          <Form.Label>New Email</Form.Label>
+          <Form.Control
+            value={email}
+            type='email'
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <div className='mb-3'>
+          <Button type='submit'>Submit</Button>
+        </div>
+      </form>
+    );
+  }
+
+  function confirmPasswordForm() {
+    return (
+      <form onSubmit={confirmPasswordToSeeUserInfoHandler}>
+        <Form.Group className='mb-3' controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            value={password}
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <div className='mb-3'>
+          <Button type='submit'>Submit</Button>
+        </div>
+      </form>
+    );
+  }
 }
