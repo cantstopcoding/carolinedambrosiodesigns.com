dbe63eefa6bce3c1b1f47bcafd35e28f3b507392
cantstopcoding <jonathan_koss@yahoo.com>
2022-09-06 19:37:30 -0400

    add emailVerified attribute

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    emailVerified: { type: Boolean, required: true, default: false },
     isAdmin: { type: Boolean, default: false, required: true },
   },
   {
