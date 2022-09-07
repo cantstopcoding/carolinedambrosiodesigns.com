35748284ad6d32d14db7c25891af4df907c4815d
cantstopcoding <jonathan_koss@yahoo.com>
2022-09-07 07:36:13 -0400

    create form in EmailVerifyScreen

--- frontend/src/screens/EmailVerifyScreen.js
+++ frontend/src/screens/EmailVerifyScreen.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 export default function EmailVerifyScreen() {
-  return <div>EmailVerifyScreen</div>;
+  const [otp, setOtp] = useState('');
+
+  const verifyOtpHandler = async (e) => {};
+
+  return (
+    <div className='container small-container'>
+      <form onSubmit={verifyOtpHandler}>
+        <h1>We sent you a code</h1>
+        <p>Enter it below to verify your email.</p>
+        <Form.Group className='mb-3' controlId='otp'>
+          <Form.Label>Verification Code</Form.Label>
+          <Form.Control value={otp} onChange={(e) => setOtp(e.target.value)} />
+        </Form.Group>
+        <div className='mb-3'>
+          <Button type='submit'>Proceed</Button>
+        </div>
+      </form>
+    </div>
+  );
 }
