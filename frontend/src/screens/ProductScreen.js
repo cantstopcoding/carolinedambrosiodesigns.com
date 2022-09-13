8ce7e4dd50fdb183f4e1c7e930d1636007975eec
cantstopcoding <jonathan_koss@yahoo.com>
2022-09-13 18:16:57 -0400

    delete comments and unused code for reviews

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,20 +1,17 @@
 import axios from 'axios';
 import { useContext, useEffect, useReducer, useRef, useState } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import Form from 'react-bootstrap/Form';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
-import Rating from '../components/Rating';
 import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
 import { getError } from '../utils';
 import { Store } from '../Store';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import { toast } from 'react-toastify';
 import { saveAs } from 'file-saver';
 
@@ -55,12 +52,11 @@ function ProductScreen() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { cart, userInfo } = state;
 
-  const [{ loading, error, product, loadingCreateReview }, dispatch] =
-    useReducer(reducer, {
-      product: [],
-      loading: true,
-      error: '',
-    });
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: '',
+  });
 
   const userOrders = userOrderData.map((o) => o.orderItems).flat();
   const userBoughtThisProduct = userOrders
@@ -187,12 +183,6 @@ function ProductScreen() {
                 </Button>
               )}
             </ListGroup.Item>
-            {/* <ListGroup.Item>
-              <Rating
-                rating={product.rating}
-                numReviews={product.numReviews}
-              ></Rating>
-            </ListGroup.Item> */}
             <ListGroup.Item>Pirce : ${product.price}</ListGroup.Item>
             <ListGroup.Item>
               <Row xs={1} md={2} className='g-2'>
@@ -255,73 +245,7 @@ function ProductScreen() {
           </Card>
         </Col>
       </Row>
-      <div className='my-3'>
-        {/* <h2 ref={reviewsRef}>Reviews</h2>
-        <div className='mb-3'>
-          {product.reviews.length === 0 && (
-            <MessageBox>There is no review</MessageBox>
-          )}
-        </div> */}
-        {/* <ListGroup>
-          {product.reviews.map((review) => (
-            <ListGroup.Item key={review._id}>
-              <strong>{review.name}</strong>
-              <Rating rating={review.rating} caption=' '></Rating>
-              <p>{review.createdAt.substring(0, 10)}</p>
-              <p>{review.comment}</p>
-            </ListGroup.Item>
-          ))}
-        </ListGroup> */}
-        {/* <div className='my-3'>
-          {userInfo ? (
-            <form onSubmit={submitHandler}>
-              <h2>Write a customer review</h2>
-              <Form.Group className='mb-3' controlId='rating'>
-                <Form.Label>Rating</Form.Label>
-                <Form.Select
-                  aria-label='Rating'
-                  value={rating}
-                  onChange={(e) => setRating(e.target.value)}
-                >
-                  <option value=''>Select...</option>
-                  <option value='1'>1- Poor</option>
-                  <option value='2'>2- Fair</option>
-                  <option value='3'>3- Good</option>
-                  <option value='4'>4- Very good</option>
-                  <option value='5'>5- Excelent</option>
-                </Form.Select>
-              </Form.Group>
-              <FloatingLabel
-                controlId='floatingTextarea'
-                label='Comments'
-                className='mb-3'
-              >
-                <Form.Control
-                  as='textarea'
-                  placeholder='Leave a comment here'
-                  value={comment}
-                  onChange={(e) => setComment(e.target.value)}
-                />
-              </FloatingLabel>
-
-              <div className='mb-3'>
-                <Button disabled={loadingCreateReview} type='submit'>
-                  Submit
-                </Button>
-                {loadingCreateReview && <LoadingBox></LoadingBox>}
-              </div>
-            </form>
-          ) : (
-            <MessageBox>
-              Please{' '}
-              <Link to={`/signin?redirect=/product/${product.slug}`}>
-                Sign In
-              </Link>{' '}
-              to write a review
-            </MessageBox>
-          )}
-        </div> */}
-      </div>
+      <div className='my-3'></div>
     </div>
   );
 }
