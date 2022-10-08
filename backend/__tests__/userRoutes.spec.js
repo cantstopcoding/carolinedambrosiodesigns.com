2edb93852a05f14dd23744c84bbebb7bfd200c61
cantstopcoding <jonathan_koss@yahoo.com>
2022-10-08 12:40:05 -0400

    refactor response

--- backend/__tests__/userRoutes.spec.js
+++ backend/__tests__/userRoutes.spec.js
@@ -6,6 +6,14 @@ beforeEach(() => {
   return User.deleteMany({});
 });
 
+async function postRequest(password) {
+  return await request(app).post('/api/users/signup').send({
+    name: 'mike',
+    email: 'm@m.com',
+    password: password,
+  });
+}
+
 describe('User Signup', () => {
   it('should respond with 200 status code with correct credenntials', async () => {
     const response = await request(app).post('/api/users/signup').send({
@@ -17,11 +25,7 @@ describe('User Signup', () => {
   });
 
   it('should have password be at least 8 characters', async () => {
-    const response = await request(app).post('/api/users/signup').send({
-      name: 'mike',
-      email: 'm@m.com',
-      password: 'Mikey1!',
-    });
+    const response = await postRequest('Mikey1!');
 
     expect(response.statusCode).toBe(400);
     expect(response.body.message).toBe(
@@ -30,11 +34,7 @@ describe('User Signup', () => {
   });
 
   it('should have password with at least one number', async () => {
-    const response = await request(app).post('/api/users/signup').send({
-      name: 'mike',
-      email: 'm@m.com',
-      password: 'Mikeymike!',
-    });
+    const response = await postRequest('Mikeymike!');
 
     expect(response.statusCode).toBe(400);
     expect(response.body.message).toBe(
@@ -43,11 +43,7 @@ describe('User Signup', () => {
   });
 
   it('should have password with at least one uppercase letter', async () => {
-    const response = await request(app).post('/api/users/signup').send({
-      name: 'mike',
-      email: 'm@m.com',
-      password: 'mikeymike1!',
-    });
+    const response = await postRequest('mikeymike1!');
 
     expect(response.statusCode).toBe(400);
     expect(response.body.message).toBe(
@@ -56,11 +52,7 @@ describe('User Signup', () => {
   });
 
   it('should have password with at least one special character', async () => {
-    const response = await request(app).post('/api/users/signup').send({
-      name: 'mike',
-      email: 'm@m.com',
-      password: 'Mikeymike1',
-    });
+    const response = await postRequest('Mikeymike1');
 
     expect(response.statusCode).toBe(400);
     expect(response.body.message).toBe(
