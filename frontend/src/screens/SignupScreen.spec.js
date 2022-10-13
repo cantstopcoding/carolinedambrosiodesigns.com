9370e4b3c1465587b9e693b361871296c9664fb7
cantstopcoding <jonathan_koss@yahoo.com>
2022-10-13 19:27:55 -0400

    SignupScreen.spec.js: finish testing "Layout"

--- frontend/src/screens/SignupScreen.spec.js
+++ frontend/src/screens/SignupScreen.spec.js
@@ -24,9 +24,47 @@ describe('SignupScreen', () => {
     });
 
     it('has username input', () => {
+      expectInputToBeInDocument('Username');
+    });
+
+    it('has email input', () => {
+      expectInputToBeInDocument('Email');
+    });
+
+    it('has confirm email input', () => {
+      expectInputToBeInDocument('Confirm Email');
+    });
+
+    it('has password input', () => {
+      expectInputToBeInDocument('Password');
+    });
+
+    it('has confirm password input', () => {
+      expectInputToBeInDocument('Confirm Password');
+    });
+
+    function expectInputToBeInDocument(labelText) {
       render(<MockSignupScreen />);
-      const input = screen.getByLabelText('Username');
+      const input = screen.getByLabelText(labelText);
       expect(input).toBeInTheDocument();
+    }
+
+    it('has sign up button', () => {
+      render(<MockSignupScreen />);
+      const button = screen.getByRole('button', { name: 'Sign Up' });
+      expect(button).toBeInTheDocument();
+    });
+
+    it('has "Already have an account?" question', () => {
+      render(<MockSignupScreen />);
+      const question = screen.getByText('Already have an account?');
+      expect(question).toBeInTheDocument();
+    });
+
+    it('has a link to signin page', () => {
+      render(<MockSignupScreen />);
+      const link = screen.getByRole('link', { name: 'Sign In' });
+      expect(link).toBeInTheDocument();
     });
   });
 });
